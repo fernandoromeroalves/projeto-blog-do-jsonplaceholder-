d1df62f708b4139ea25bf025065a0b6b2b93b99a
fernandoromeroalves <133946667+fernandoromeroalves@users.noreply.github.com>
2023-08-16 15:50:11 -0300

    fim

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+//https://jsonplaceholder.typicode.com/posts
+
+async function readPost() {
+    let postArea = document.querySelector('.posts');
+    postArea.innerHTML = 'Carregando...'
+
+    let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let json = await response.json();
+
+    if(json.length > 0) {
+        postArea.innerHTML = '';
+
+        for(let i in json) {
+            let postHtml = `<div><h1>${json[i].title}</h1>${json[i].body}</hr></div>`;
+            postArea.innerHTML += postHtml;
+        }
+    } else {
+        postArea.innerHTML = 'Nenhum post para exibir;'
+    }
+}
+
+async function addNewPost(title, body){
+    await fetch(
+        'https://jsonplaceholder.typicode.com/posts', {
+        method:'POST',
+        Headers: {
+            'Content-Type': 'applicatio/jso'
+        },
+        body: JSON.stringify({
+            title: title,
+            body:body,
+            userId: 2
+        })
+    }
+    );
+    document.querySelector('#titleField').value = '';
+    document.querySelector('#bodyField').value = '';
+
+    readPost();
+};
+
+document.querySelector('#insertButton').addEventListener('click', () => {
+    let title = document.querySelector('#titleField').value;
+    let body = document.querySelector('#bodyField').value;
+
+    if(title && body) {
+        addNewPost(title, body)
+    } else {
+        alert('preencha todos os campos')
+    }
+})
+
+readPost()
